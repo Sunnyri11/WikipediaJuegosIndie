7e02557a34466481e634743f916f61dce302f352
Sunnyri11 <jhoelmauro1995@gmail.com>
2025-12-03 19:25:49 -0400

    Se añadio el paginador en la pagina principal

--- mynd.js
+++ mynd.js
@@ -3,3 +3,21 @@
       el.classList.remove('oculto');
     });
   }, 10000);
+document.addEventListener("DOMContentLoaded", () => {
+  const fichas = [...document.querySelectorAll("#contenedor-juegos .juegos")];
+  const porPagina = 6;
+  const totalPaginas = Math.ceil(fichas.length / porPagina);
+  const paginador = document.getElementById("paginador");
+  const mostrar = (pagina) => {
+    fichas.forEach((f, i) => {
+      f.style.display = (i >= (pagina-1)*porPagina && i < pagina*porPagina) ? "inline-block" : "none";
+    });
+  };
+  for (let i = 1; i <= totalPaginas; i++) {
+    let pasar = document.createElement("button");
+    pasar.textContent = i;
+    pasar.onclick = () => mostrar(i);
+    paginador.appendChild(pasar);
+  }
+  mostrar(1); 
+});
